8e06569f9785b81d2ee787af2da478402ba4c4ed
VanessaBecerra95 <vanessa.becerra.zamorano@ciisa.cl>
2024-06-14 22:23:44 -0400

    update placement CalculadoraTest

--- src/test/java/cl/praxis/repository/CalculadoraTest.java
+++ src/test/java/cl/praxis/repository/CalculadoraTest.java
@@ -1,4 +1,4 @@
-package cl.praxis;
+package cl.praxis.repository;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
